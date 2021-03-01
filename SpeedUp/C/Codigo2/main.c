6cd9ddc211d50344b036a149d4a61aa920a3855b
Matheus Victor Alves da Silva <48606348+MatheusXDs@users.noreply.github.com>
2021-03-01 20:56:31 -0300

    Update main.c

--- SpeedUp/C/Codigo2/main.c
+++ SpeedUp/C/Codigo2/main.c
@@ -1,8 +1,14 @@
+#include "TestaPrimo.h"
+#include <time.h>
+
 int main() {
   int num, i, resultado = 0;
   
-  num = 7;
-  
+  num = 2147483647;
+
+  clock_t tempo;
+  tempo = clock();
+
   for (i = 2; i <= num / 2; i++) {
       if (num % i == 0) {
         resultado=0;
@@ -10,14 +16,11 @@ int main() {
       }
       resultado=1;
   }
-
-
+  
   //  Verifica o tempo de execução.
-  clock_t tempo;
-  tempo = clock();
   printf("%i\n",resultado);
   
-  printf("Tempo:%f\n",(clock() - tempo) / (double)CLOCKS_PER_SEC);
+  printf("Tempo:%f\n",(clock() - tempo)*1000 / (double)CLOCKS_PER_SEC);
 
   return 0;
 }
