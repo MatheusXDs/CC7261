eb2e1f380d976ecb3164e716da2f45a5351aadbb
Matheus Victor Alves da Silva <48606348+MatheusXDs@users.noreply.github.com>
2021-03-01 20:39:47 -0300

    Update main.c

--- SpeedUp/C/Codigo2/main.c
+++ SpeedUp/C/Codigo2/main.c
@@ -10,14 +10,13 @@ int main() {
       }
       resultado=1;
   }
-  printf("%i\n",resultado);
+
 
   //  Verifica o tempo de execução.
   clock_t tempo;
   tempo = clock();
-
-    // código de teste
-  for(int i = 0; i < 999999999; ++i){}
+  printf("%i\n",resultado);
+  
   printf("Tempo:%f\n",(clock() - tempo) / (double)CLOCKS_PER_SEC);
 
   return 0;
