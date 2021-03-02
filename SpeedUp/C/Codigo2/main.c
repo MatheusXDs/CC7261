fe2e8c23e403401ff4ad5fc2ca4d53a2b094caf6
Matheus Victor Alves da Silva <48606348+MatheusXDs@users.noreply.github.com>
2021-03-02 15:59:18 -0300

    Update main.c

--- SpeedUp/C/Codigo2/main.c
+++ SpeedUp/C/Codigo2/main.c
@@ -3,24 +3,29 @@
 
 int main() {
   int num, i, resultado = 0;
+  float vetor[30], media;
   
-  num = 2147483647;
+  num = 7;
+  for(int f=0 ; f < 30; f++){
+    clock_t tempo;
+    tempo = clock();
 
-  clock_t tempo;
-  tempo = clock();
-
-  for (i = 2; i <= num / 2; i++) {
+    for (i = 2; i <= num / 2; i++) {
       if (num % i == 0) {
         resultado=0;
         break;
       }
       resultado=1;
+    }
+    
+    printf("%i\n",resultado);
+    
+    printf("Tempo:%f\n",(clock() - tempo)*1000 / (double)CLOCKS_PER_SEC);
+    vetor[f] = ((clock() - tempo)*1000 / (double)CLOCKS_PER_SEC);
+    media += vetor[f];
   }
-  
-  //  Verifica o tempo de execução.
-  printf("%i\n",resultado);
-  
-  printf("Tempo:%f\n",(clock() - tempo)*1000 / (double)CLOCKS_PER_SEC);
 
+  media = media/30;
+  printf("%f", media);
   return 0;
 }
